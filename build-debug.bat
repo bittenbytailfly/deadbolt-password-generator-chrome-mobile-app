148db9ecbd1a01e2c02a87bbf5b4843330da15dd
Ed Carter <bittenbytailfly@server.fake>
2014-07-29 00:27:53 +0100

    Tabbed Settings

---
+++ build-debug.bat
@@ -0,0 +1,6 @@
+call vulcanize -o app/build.htm app/deadbolt.htm --csp
+cd deadbolt
+call cca prepare
+cd platforms/android/cordova
+call build --debug
+cd ../../../../
---
+++ install-debug.bat
@@ -0,0 +1,7 @@
+cd deadbolt/platforms/android/bin
+call adb install deadboltpasswordgenerator-debug.apk
+cd ../../../../
+
+
+
+
